def4dd649b5632de4285d082f923cf8d4dc7b7c3
Olarewaju Temtope Albert <tmtpalbert@gmail.com>
2022-08-29 17:13:37 +0100

    games
    fix game

---
+++ New Challenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timeCounter = 0;
+    public float dogSpawnDelay = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter = timeCounter
+            + Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && (timeCounter > dogSpawnDelay))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeCounter = 0;
+        }
+
+    }
+
+}
